d9ab709076efaa6d5547c9728c4fe36c12581eb3
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2021-06-24 17:28:01 +0200

    Add auxiliary uniformly accelerated movement function

--- tests/testthat/test-kinematics.R
+++ tests/testthat/test-kinematics.R
@@ -1,6 +1,4 @@
-context('Kinematics')
-
-## Auxiliary functions
+## ====== Auxiliary functions ======
 
 #' Expect equal for a constant and a vector
 #'
@@ -16,19 +14,39 @@ expect_equal_constvect <- function(constant, vector, ...) {
   expect_equal(rep(constant, length(vector)), vector, ...)
 }
 
+#' 2D uniformly accelerated movement
+#'
+#' Generates a 2D accelerated movement time series from its analytic expression
+#' \url{https://en.wikipedia.org/wiki/Acceleration#Uniform_acceleration}
+#'
+#' @param t The vector of times
+#' @param r_0 Initial position
+#' @param v_0 Initial speed
+#' @param a_0 Initial acceleration
+#'
+#' @return A dataframe with the positions and times
+#'
+uniformly_accelerated_mov <- function(t, r_0, v_0, a_0 = c(0, 0)) {
+  x <- r_0[1] + v_0[1] * t + a_0[1] / 2 * t^2
+  y <- r_0[2] + v_0[2] * t + a_0[2] / 2 * t^2
+
+  return(data.frame(x, y))
+}
+
+## ====== Tests ======
+context('Kinematics')
+
 test_that('Speed',
           {
-            # Generate movement with uniform speed (https://en.wikipedia.org/wiki/Acceleration#Uniform_acceleration)
-            x_0 <- 0 # Initial...
-            y_0 <- 1 # ... positions
+            # Generate movement with uniform speed
+            r_0 <- c(0, 1) # Initial positions
             v_expected <- c(1.2, 0.8) # Initial (and constant) speed
-
             t <- seq(0, 1, by=0.05)
-            x <- x_0 + v_expected[1]*t
-            y <- y_0 + v_expected[2]*t
+
+            mov <- uniformly_accelerated_mov(t, r_0, v_expected)
 
             # Estimate the speeds from the generated data
-            vs_estimated <- speed(t, x, y)
+            vs_estimated <- speed(t, mov$x, mov$y)
 
             # Check that the estimates are correct
             expect_equal_constvect(v_expected[1], vs_estimated$vx)
@@ -38,19 +56,17 @@ test_that('Speed',
 
 test_that('Acceleration',
           {
-            # Generate movement with uniform acceleration (https://en.wikipedia.org/wiki/Acceleration#Uniform_acceleration)
-            x_0 <- 0 # Initial...
-            y_0 <- 1 # ... positions
+            # Generate movement with uniform acceleration
+            r_0 <- c(0, 1) # Initial positions
             v_0 <- c(1.2, 0.8) # Initial speed
             a_expected <- c(0.0, -9.8) # Initial (and constant) acceleration
-
             t <- seq(0, 1, by=0.05)
-            x <- x_0 + v_0[1]*t + a_expected[1]*t^2/2
-            y <- y_0 + v_0[2]*t + a_expected[2]*t^2/2
+
+            mov <- uniformly_accelerated_mov(t, r_0, v_0, a_expected)
 
             # Estimate the speeds from the generated data
-            vs_estimated <- speed(t, x, y)
-            as_estimated <- accel(t, x, y)
+            vs_estimated <- speed(t, mov$x, mov$y)
+            as_estimated <- accel(t, mov$x, mov$y)
 
             # Check that the estimates are correct
             expect_equal_constvect(a_expected[1], as_estimated$ax)
