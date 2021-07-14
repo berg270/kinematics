802df1d5a968d8ea781db06623331df451ca51c1
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2021-07-14 15:48:19 +0200

    Add example.rda

---
+++ data/data.R
@@ -0,0 +1,12 @@
+#' Example data set
+#'
+#' Experimental sample of 3000 positions of a macroinvertebrate
+#'
+#' @format A data frame with 3000 observations of:
+#' \describe{
+#'   \item{x}{horizontal position}
+#'   \item{y}{vertical position}
+#'   \item{t}{time}
+#'   ...
+#' }
+"example"
